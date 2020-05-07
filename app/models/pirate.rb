cb9bf09e24bce2cdb03a789535ea467c38167618
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-07 22:03:35 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+  
+  def initialize(name, weight, height)
+    @name = params[:name] 
+    @weight = params[:weight]
+    @height = params[:height] 
+    save 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
 end
